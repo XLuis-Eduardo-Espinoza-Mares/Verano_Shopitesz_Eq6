a42f2f0a755f95e4f6c8b8d3c88c263a8e135cb5
Espinoza <lespinoza380@accitesz.com>
2021-07-07 15:34:29 -0500

    app

---
+++ Shopitesz/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def inicio():
+    #return "Bienvenido a la tienda en linea Shipitesz
+    return render_template('principal.html')
+
+@app.route("/validarSesion")
+def validarSesion():
+    return render_template('usuarios/login.html')
+
+@app.route("/login",methods=['POST'])
+def login():
+    correo=request.form['correo']
+    return "Validando al usuario"+correo
