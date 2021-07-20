c6e59ca806806c52cbc851345fa98e6086937864
AaronMarquezF <amarquez025@accitesz.com>
2021-07-20 04:14:16 -0500

    modelo

--- Shopitesz/app.py
+++ Shopitesz/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, request
+from datetime import timedelta
+
+from flask import Flask,render_template,request,redirect,url_for,flash,session,abort
 from flask_bootstrap import Bootstrap
+from modelo.Dao import db,Categoria
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://user_shopiteszpractica:Shopit3sz.123@localhost/shopiteszpractica'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route("/")
 def inicio():
     #return "Bienvenido a la tienda en linea Shipitesz
     return render_template('principal.html')
-
 @app.route("/validarSesion")
 def validarSesion():
     return render_template('usuarios/login.html')
@@ -48,9 +52,20 @@ def Descripcion2():
 def Descripcion3():
     return render_template('productos/ProductosDescripcion/CAMISAGAMER.html')
 
+
+
+@app.route("/Categorias")
+def consultaCategorias():
+    cat=Categoria()
+    render_template('categorias/consultaGeneral.html',categorias=cat.consultaGeneral())
+
+
+
 @app.route("/login",methods=['POST'])
 def login():
     correo=request.form['correo']
     return "Validando al usuario"+correo
+
 if __name__=='__main__':
+    db.init_app(app) #Inicializando BD - pasa configuracion de la URL de la BD
     app.run(debug=True)
